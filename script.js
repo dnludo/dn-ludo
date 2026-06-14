4d32ed92defcbc337eafcae93f515229f40d30a4
dnludo <dnludo7@gmail.com>
2026-06-14 17:34:36 +0530

    Update script.js

--- script.js
+++ script.js
@@ -54,23 +54,38 @@ yellowToken.classList.remove("active-token");
 blueToken.classList.remove("active-token");
 if(players[currentPlayer] === "Red"){
 redHome.classList.add("active-red");
+redToken.style.background="#ff0000";
+greenToken.style.background="#e0e0e0";
+yellowToken.style.background="#e0e0e0";
+blueToken.style.background="#e0e0e0";
 redToken.classList.add("active-token");
 }
 
 if(players[currentPlayer] === "Green"){
 greenHome.classList.add("active-green");
+redToken.style.background="#e0e0e0";
+greenToken.style.background="#00ff00";
+yellowToken.style.background="#e0e0e0";
+blueToken.style.background="#e0e0e0";
 greenToken.classList.add("active-token");
 }
 
 if(players[currentPlayer] === "Yellow"){
 yellowHome.classList.add("active-yellow");
+redToken.style.background="#e0e0e0";
+greenToken.style.background="#e0e0e0";
+yellowToken.style.background="#ffff00";
+blueToken.style.background="#e0e0e0";
 yellowToken.classList.add("active-token");
 }
 
 if(players[currentPlayer] === "Blue"){
 blueHome.classList.add("active-blue");
+redToken.style.background="#e0e0e0";
+greenToken.style.background="#e0e0e0";
+yellowToken.style.background="#e0e0e0";
+blueToken.style.background="#0099ff";
 blueToken.classList.add("active-token");
 }
-
 }
 updateHighlight();
