44abb70a83929156a13a07a89b9a762c4ba63c68
dnludo <dnludo7@gmail.com>
2026-06-15 22:25:55 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,7 @@
-console.log("DN Ludo Loaded");
+const diceBtn = document.getElementById("rollDice");
+const diceValue = document.getElementById("diceValue");
+
+diceBtn.addEventListener("click", () => {
+    const roll = Math.floor(Math.random() * 6) + 1;
+    diceValue.innerText = roll;
+});
