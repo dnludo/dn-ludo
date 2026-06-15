604c269df2f4fd7cef54c0eb3b1fdb2606868192
dnludo <dnludo7@gmail.com>
2026-06-15 22:46:04 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,7 +1,1 @@
-const diceBtn = document.getElementById("rollDice");
-const diceValue = document.getElementById("diceValue");
 
-diceBtn.addEventListener("click", () => {
-    const roll = Math.floor(Math.random() * 6) + 1;
-    diceValue.innerText = roll;
-});
