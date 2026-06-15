3afc0c3839574a1f18d06b112de9c6363c71cae7
dnludo <dnludo7@gmail.com>
2026-06-16 00:56:33 +0530

    Update script.js

--- script.js
+++ script.js
@@ -36,11 +36,51 @@ document.getElementById("app").innerHTML = `
 <div class="menu-screen">
 <h1>Select Players</h1>
 
-<button class="menu-btn">2 Players</button>
-<button class="menu-btn">3 Players</button>
-<button class="menu-btn">4 Players</button>
+<button class="menu-btn" onclick="startGame(2)">2 Players</button>
+<button class="menu-btn" onclick="startGame(3)">3 Players</button>
+<button class="menu-btn" onclick="startGame(4)">4 Players</button>
 
 <button class="menu-btn" onclick="showMenu()">Back</button>
+
+</div>
+`;
+}
+
+function startGame(players) {
+document.getElementById("app").innerHTML = `
+<div class="menu-screen">
+
+<h1>DN LUDO</h1>
+
+<h2>${players} Players Selected</h2>
+
+<button class="menu-btn" id="startMatchBtn">
+Start Match
+</button>
+
+<button class="menu-btn" onclick="showPlayerSelection()">
+Back
+</button>
+
+</div>
+`;
+
+document
+.getElementById("startMatchBtn")
+.addEventListener("click", showBoard);
+}
+
+function showBoard() {
+document.getElementById("app").innerHTML = `
+<div class="menu-screen">
+
+<h1>LUDO BOARD</h1>
+
+<p>Board Coming Soon</p>
+
+<button class="menu-btn" onclick="showMenu()">
+Main Menu
+</button>
 
 </div>
 `;
