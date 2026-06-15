cb0305b8f0598f8cf46a00f601a7472b0cf5cead
dnludo <dnludo7@gmail.com>
2026-06-15 23:12:15 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,16 @@
+document.getElementById("app").innerHTML = `
+<div class="splash-screen">
+    <h1>DN LUDO</h1>
+    <p>Presented by HK Games & Co.</p>
 
+    <button id="playBtn">
+        PLAY NOW
+    </button>
+</div>
+`;
+
+document
+.getElementById("playBtn")
+.addEventListener("click",function(){
+    alert("DN Ludo Main Menu Coming Soon");
+});
