a68e8a5f9393c1c92456a9f9e2df5d64198dcd0b
dnludo <dnludo7@gmail.com>
2026-06-16 00:24:15 +0530

    Update script.js

--- script.js
+++ script.js
@@ -20,19 +20,18 @@ document.getElementById("app").innerHTML = `
 <div class="menu-screen">
 <h1>DN LUDO</h1>
 
-<button class="menu-btn">Play Game</button>
+<button class="menu-btn" id="playGameBtn">Play Game</button>
 <button class="menu-btn">Players</button>
 <button class="menu-btn">Settings</button>
 <button class="menu-btn">About</button>
 
 </div>
 `;
-}
 
 document
 .getElementById("playBtn")
 .addEventListener("click", showMenu);
-
+}
 function showPlayerSelection() {
 document.getElementById("app").innerHTML = `
 <div class="menu-screen">
