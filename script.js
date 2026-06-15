16960e1dffbc439fa33ee3c3ce9c70e88c8071e6
dnludo <dnludo7@gmail.com>
2026-06-16 00:12:48 +0530

    Update script.js

--- script.js
+++ script.js
@@ -32,3 +32,20 @@ document.getElementById("app").innerHTML = `
 document
 .getElementById("playBtn")
 .addEventListener("click", showMenu);
+
+function showPlayerSelection() {
+document.getElementById("app").innerHTML = `
+<div class="menu-screen">
+<h1>Select Players</h1>
+
+<button class="menu-btn" onclick="startGame(2)">2 Players</button>
+
+<button class="menu-btn" onclick="startGame(3)">3 Players</button>
+
+<button class="menu-btn" onclick="startGame(4)">4 Players</button>
+
+<button class="menu-btn" onclick="showMenu()">Back</button>
+
+</div>
+`;
+}
